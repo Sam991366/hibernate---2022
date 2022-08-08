3f9b8e93392ca4dfe5edb800d05c55b0b7f48042
samarthladani133 <samarthladani133@gmail.com>
2022-08-08 10:40:51 +0530

    first commit

---
+++ src/main/java/com/bean/RoleBean.java
@@ -0,0 +1,27 @@
+package com.bean;
+
+//ORM 
+//class object == relation  == map 
+
+//role table 
+public class RoleBean {
+	private int roleId;
+	private String roleName;
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+}
